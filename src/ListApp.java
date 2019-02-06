2ad9f9d5cb1e9c66eb0c2e4157b17b338266d69f
Maltheos <krzysztof.grabka@gmail.com>
2019-02-06 16:10:26 -0500

    initial commit, nothing complete just to create repo

---
+++ src/ListApp.java
@@ -0,0 +1,40 @@
+public class ListApp {
+
+	public static void main(String[] args) {
+		testArrayList();
+		testLinkedList();
+	}
+	
+	private static void testArrayList() {
+		MyArrayList list = new MyArrayList();
+		
+		list.addAtBeginning("B");
+		list.addAtBeginning("A");
+		list.addAtEnd("C");
+		list.addAtEnd("D");
+		list.removeFromBeginning();
+		list.removeFromEnd();
+		
+		System.out.println(list);
+		System.out.println(list.size());
+		System.out.println(list.get(0));
+		System.out.println(list.get(1));
+	}
+	
+	private static void testLinkedList() {
+		MyLinkedList list = new MyLinkedList();
+		
+		list.addAtBeginning("B");
+		list.addAtBeginning("A");
+		list.addAtEnd("C");
+		list.addAtEnd("D");
+		list.removeFromBeginning();
+		list.removeFromEnd();
+		
+		System.out.println(list);
+		System.out.println(list.size());
+		System.out.println(list.get(0));
+		System.out.println(list.get(1));
+	}
+
+}
